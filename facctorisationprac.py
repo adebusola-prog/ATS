9cc9b34f0437c2a73f1589787a3b054949d8a59d
Adebusola <aadeyeye@afexnigeria.com>
2022-06-16 10:05:50 -0700

    addedclasswork

---
+++ facctorisationprac.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+
+a = int(input("what is a?"))
+b = int(input("what is b?"))
+c = int(input("what is c?"))
+Factor_list = []
+product = a * c
+factor + factor == b
+
+if i in range(-product, product + 1):
+    a != 0
