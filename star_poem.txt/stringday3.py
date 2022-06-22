5d239ed7080ba9e7f35740eed62b395d8e09598d
Adebusola <aadeyeye@afexnigeria.com>
2022-06-22 12:54:24 -0700

    week 3 files

---
+++ star_poem.txt/stringday3.py
@@ -0,0 +1,31 @@
+https://github.com/adebusola-prog/ATS.git
+#Write a function that takes a two strings as an argument and prints if the first string is a substring of the second.
+x = "Hello" in "Hello world!"
+print(x)
+
+#Write a function that converts a string to uppercase and returns the result.
+message = "hello dear"
+x = message.upper()
+print(x)
+
+#Write a function that converts a string to lowercase and returns the result.
+greetings = "GOOD MORNING"
+y = greetings.lower()
+print(y)
+
+#Write a function that converts a string to titlecase and returns the result.
+noun = "boys and girls"
+x = noun.title()
+print(x)
+
+#Write a function that converts the first letter of each word in a sentence to uppercase.
+question = "what do you want to eat tommorrow and next-tomorrow"
+z = question.title()
+print(z)
+
+#Write a function that checks if a string contains both alphabets and numbers.
+add = "ABCD123"
+x = add.isalnum()
+print(x)
+
+
